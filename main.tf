e5caa6841aed00798c5b2f8b1e4f1e7c505de894
Gowrc2807 <106998367+Gowrc2807@users.noreply.github.com>
2022-06-06 12:08:31 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+variable "hw" {
+type = string
+default = "t2.nano"
+}
+
+resource "aws_instance" "myawsserver1" {
+  ami = "ami-0603cbe34fd08cb81"
+  instance_type = var.hw
+  tags = {
+    Name = "shan-vm1"
+    Env = "Dev"
+  }
+}
